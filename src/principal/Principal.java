2d4c4177ccbb543d390e6d7a310c5fefda76a12c
AlexandroNazvalskyy <alexandro.nazvalskyy@educa.madrid.org>
2025-03-25 21:06:24 +0100

    Nuevo mensaje

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Alexandro Nazvalskyy Venher");
+		System.out.println("Tengo 80 castañas");
 	}
 }
